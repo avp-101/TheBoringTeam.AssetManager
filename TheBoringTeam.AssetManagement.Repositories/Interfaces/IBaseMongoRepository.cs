0c7405a9e71e8383a7e4beb672fed868cf194334
Adi <adrian.petre97@gmail.com>
2018-10-27 14:45:14 +0300

    added mongo repositories

---
+++ TheBoringTeam.AssetManagement.Repositories/Interfaces/IBaseMongoRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace TheBoringTeam.AssetManagement.Repositories.Interfaces
+{
+    public interface IBaseMongoRepository<TBaseEntity> where TBaseEntity : IIdentifiable, ITrackable
+    {
+        IEnumerable<TBaseEntity> Get();
+        IEnumerable<TBaseEntity> Get(Expression<Func<TBaseEntity, bool>> predicate);
+        void Insert(TBaseEntity entity);
+        void Insert(IEnumerable<TBaseEntity> entities);
+        void Update(TBaseEntity entity);
+        void Delete(TBaseEntity entity);
+        void Delete(IEnumerable<TBaseEntity> entities);
+        void Delete(string id);
+        void Delete(IEnumerable<string> ids);
+    }
+}
