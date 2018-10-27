8e6e25fb0798efe8070c4eb98c7aaf1b1bc361ca
Cristina Dolea <dolea.christine@gmail.com>
2018-10-27 14:53:03 +0300

    commit01

--- TheBoringTeam.AssetManagement.Repositories/Repositories/GenericRepository.cs
+++ TheBoringTeam.AssetManagement.Repositories/Repositories/GenericRepository.cs
@@ -1,10 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using TheBoringTeam.AssetManagement.Models.Interfaces;
 
 namespace TheBoringTeam.AssetManagement.Repositories.Repositories
 {
-    public class GenericRepository
+    public class GenericRepository<TEntity> : IGenericRepository<TEntity> where TEntity : class, IIdentifiable, ITrackable
     {
     }
 }
---
+++ TheBoringTeam.AssetManagement.Repositories/Repositories/Interfaces/IGenericRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TheBoringTeam.AssetManagement.Models.Interfaces;
+
+namespace TheBoringTeam.AssetManagement.Repositories.Repositories.Interfaces
+{
+    public interface IGenericRepository<TEntity> where TEntity : class, IIdentifiable, ITrackable
+    {
+
+    }
+}
