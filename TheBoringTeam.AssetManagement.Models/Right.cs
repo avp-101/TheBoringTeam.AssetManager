cf0656a6e66e734fc5fe67f8c91ed786cfb49e13
Cristina Dolea <dolea.christine@gmail.com>
2018-10-27 16:48:20 +0300

    added Role,Right

--- TheBoringTeam.AssetManagement.Models/Right.cs
+++ TheBoringTeam.AssetManagement.Models/Right.cs
@@ -1,10 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
+using TheBoringTeam.AssetManagement.Models.Interfaces;
 
 namespace TheBoringTeam.AssetManagement.Models
 {
-    public class Right
+    public class Right: IIdentifiable, ITrackable
     {
+        [Required]
+        public string Id { get; set; }
+
+        [Required]
+        public IEnumerable<string> Rights { get; set; }
+
+        [Required]
+        public DateTime CreatedOn { get; set; }
+
+        public DateTime? ModifiedOn { get; set; }
+
     }
 }
--- TheBoringTeam.AssetManagement.Models/Role.cs
+++ TheBoringTeam.AssetManagement.Models/Role.cs
@@ -1,10 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
+using TheBoringTeam.AssetManagement.Models.Interfaces;
 
 namespace TheBoringTeam.AssetManagement.Models
 {
-    public class Role
+    public class Role: IIdentifiable, ITrackable
     {
+        [Required]
+        public string Id { get; set; }
+
+        [Required]
+        public string RoleId { get; set; }
+
+        [Required]
+        public DateTime CreatedOn { get; set; }
+
+        public DateTime? ModifiedOn { get; set; }
     }
 }
