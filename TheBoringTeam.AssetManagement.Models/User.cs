069c175287fa6424f243b38837ec8e21a5a89a6d
Cristina Dolea <dolea.christine@gmail.com>
2018-10-27 14:44:29 +0300

    first commit

---
+++ TheBoringTeam.AssetManagement.Models/User.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace TheBoringTeam.AssetManagement.Models
+{
+    public class User
+    {
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        public string DisplayName { get; set; }
+
+        [Required]
+        public string Username { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+
+        [Required]
+        public string Role { get; set; }
+
+        [Required]
+        public string Right { get; set; }
+
+        [Required]
+        public DateTime CreatedOn { get; set; }
+
+        public DateTime? ModifiedOn { get; set; }
+    }
+}
---
+++ TheBoringTeam.AssetManagement.Repositories/Repositories/GenericRepository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TheBoringTeam.AssetManagement.Repositories.Repositories
+{
+    public class GenericRepository
+    {
+    }
+}
